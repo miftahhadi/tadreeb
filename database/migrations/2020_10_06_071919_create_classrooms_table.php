5ae59c2ace523bff288752e06a3f821fab48081d
Miftah Hadi S <miftahhadi@gmail.com>
2020-10-06 14:31:45 +0700

    Initiated classroom model and classroom

---
+++ database/migrations/2020_10_06_071919_create_classrooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained()
+                                        ->cascadeOnDelete();
+            $table->string('nama');
+            $table->text('deskripsi');
+            $table->timestamps();
+
+            $table->index('group_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classrooms');
+    }
+}
