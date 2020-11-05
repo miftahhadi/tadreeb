60f9563309162bc5b6277b136778ea36bb9b41ef
Miftah Hadi S <miftahhadi@gmail.com>
2020-11-05 13:12:29 +0700

    Created classroomexam_user pivot table

---
+++ database/migrations/2020_11_04_064822_create_classroomexam_user_pivot_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomexamUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classroomexam_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('classroom_exam_id')
+                    ->constrained('classroom_exam')
+                    ->onDelete('cascade');
+            $table->foreignId('user_id')
+                    ->constrained()
+                    ->onDelete('cascade');
+            $table->integer('attempt')->default(1);
+            $table->timestamp('waktu_mulai')->nullable();
+            $table->timestamp('waktu_selesai')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classroomexam_user');
+    }
+}
