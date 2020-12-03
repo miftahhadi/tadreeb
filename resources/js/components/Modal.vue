6b570f87fe06a1c4ad7b4a521f0578033d7fe06a
Miftah Hadi S <miftahhadi@gmail.com>
2020-12-03 15:09:06 +0700

    Created modal component

---
+++ resources/js/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="modal fade" 
+        :id="id " 
+        tabindex="-1" :aria-labelledby="id + 'Label'" aria-hidden="true">
+        <div class="modal-dialog" :class="modalClass" role="document">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title">
+                        <slot name="title"></slot>
+                    </h5>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <button type="button" class="btn mr-auto" data-bs-dismiss="modal">Close</button>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'modal',
+
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        classes: {
+            type: Array,
+            default: () => []
+        }
+    },
+
+    computed: {
+        modalClass() {
+            return this.classes.join(' ');
+        }
+    }
+}
+</script>
