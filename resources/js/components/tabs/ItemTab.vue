d0286e1d99f80f598641d9adf16d6900d6b1da0b
Miftah Hadi S <miftahhadi@gmail.com>
2020-12-03 15:10:19 +0700

    Created tabs component

---
+++ resources/js/components/tabs/ItemTab.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+        <ul class="nav nav-tabs">
+            <li class="nav-item" v-for="tab in tabs" :key="tab.name">
+                <a :href="tab.href" class="nav-link" :class="{ 'active': tab.isActive }" @click="selectTab(tab)">{{ tab.name }}</a>
+            </li>
+        </ul>
+
+        <div class="content-tab mt-4">
+            <slot></slot>
+        </div>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'item-tab',
+
+    data() {
+        return {
+            tabs : []
+        }
+    },
+
+    created() {
+        this.tabs = this.$children
+    },
+
+    methods: {
+        selectTab(selected) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selected.name)
+            })
+        }
+    }
+}
+</script>
---
+++ resources/js/components/tabs/TabDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tab-pane" v-show="isActive">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tab-details',
+
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+
+        selected: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+            isActive: this.selected
+        }
+    },
+    
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    }
+}
+</script>
