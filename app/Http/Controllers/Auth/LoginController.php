185c61babde19ece7efeeddf0f6e7d5ecace23c3
Miftah Hadi S <miftahhadi@gmail.com>
2020-12-09 10:06:55 +0700

    Set redirect route after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +28,18 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
+
+    public function redirectTo()
+    {
+        if (auth()->user()->can('access admin')) {
+            $this->redirectTo = route('admin.dashboard');
+        } else {
+            $this->redirectTo = route('dashboard');
+        }
+
+        return $this->redirectTo;
+    }
 
     /**
      * Create a new controller instance.
