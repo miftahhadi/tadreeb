de9891642dfa389f5ebda698f9b3c2a8cff42942
Miftah Hadi S <miftahhadi@gmail.com>
2020-12-30 15:15:56 +0700

    Show role name

--- resources/views/admin/_header-nav.blade.php
+++ resources/views/admin/_header-nav.blade.php
@@ -17,8 +17,8 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="7" r="4" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                 </span>
                 <div class="d-none d-xl-block pl-2">
-                    <div>{{ auth()->user()->nama }}</div>
-                    <div class="mt-1 small text-muted">{{ auth()->user()->getRoleNames()->first() }}</div>
+                    <div>{{ auth()->user()->name }}</div>
+                    <div class="mt-1 small text-muted">{{ auth()->user()->getFirstRole() }}</div>
                 </div>
             </a>
 
