caacbf0a913c102155e75df2ed243717e5674895
Miftah Hadi S <miftahhadi@gmail.com>
2021-02-19 09:54:00 +0700

    Init show page

--- resources/views/admin/_breadcrumb.blade.php
+++ resources/views/admin/_breadcrumb.blade.php
@@ -1,4 +1,4 @@
-<ol class="breadcrumb breadcrumb-arrows breadcrumb-alternate" aria-label="breadcrumbs">
+<ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
     <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
     @foreach ($breadcrumbs as $item)
         <li class="breadcrumb-item" aria-current="page"><a href="{{ $item['href'] }}">
--- resources/views/admin/grup/show.blade.php
+++ resources/views/admin/grup/show.blade.php
@@ -6,26 +6,15 @@
 
     @include('admin._item-header')
 
-    <div class="row">
-        <div class="col-auto">
+    <item-tab>
+        <tab-details name="Kelas" :selected="true">
             <h2>Daftar Kelas</h2>
-        </div>    
-        <div class="col-auto ml-auto">
-    
-            <button type="button" 
-                    class="btn" 
-                    data-toggle="modal" 
-                    data-target="#tambahBaru">
-                
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
-                </span> 
-                
-                Buat Kelas Baru
-            </button>  
-    
-        </div>
-    </div>
+        </tab-details>
+
+        <tab-details name="Item Bersama">
+            <h2>Daftar Item Bersama</h2>
+        </tab-details>
+    </item-tab>
 
 </div>
 @endsection
